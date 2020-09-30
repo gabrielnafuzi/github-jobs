537f8241ba03cb9936096c3051b76a1563f39f61
gabrielnafuzi <gabrielnafuzi@gmail.com>
2020-09-30 09:24:43 -0300

    changed 'to' to 'path' on route

--- src/pages/JobDetails/index.js
+++ src/pages/JobDetails/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const JobDetails = () => {
-  return <div />;
+  return <Container>
+    <h1>aaa</h1>
+  </Container>;
 };
 
 export default JobDetails;
