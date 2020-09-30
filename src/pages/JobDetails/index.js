3aa506f687787f6f3d4eab6f4b326010c13a5ac6
gabrielnafuzi <gabrielnafuzi@gmail.com>
2020-09-30 12:11:40 -0300

    job details page layout finished

--- src/pages/JobDetails/index.js
+++ src/pages/JobDetails/index.js
@@ -1,11 +1,151 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import {
+  Container,
+  Main,
+  LeftSide,
+  ArrowBack,
+  RightSide,
+  HowToApplyBlock,
+  JobHeader,
+  JobTitleBlock,
+  CreatedAt,
+  ClockIcon,
+  JobInfo,
+  CompanyLogoBlock,
+  InfoBlock,
+  LocationBlock,
+  PlanetIcon,
+  JobDescription,
+} from './styles';
 
 const JobDetails = () => {
-  return <Container>
-    <h1>aaa</h1>
-  </Container>;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <Link to="/">
+            <ArrowBack />
+            <span>Back to search</span>
+          </Link>
+
+          <HowToApplyBlock>
+            <h1>How to apply</h1>
+
+            <p>
+              Please email a copy of your resume and online portfolio to{' '}
+              <a href="mailto: wes@kasisto.com">wes@kasisto.com</a> & CC{' '}
+              <a href="mailto: eric@kasisto.com">eric@kasisto.com</a>
+            </p>
+          </HowToApplyBlock>
+        </LeftSide>
+
+        <RightSide>
+          <JobHeader>
+            <JobTitleBlock>
+              <h1>Front-End Software Engineer</h1>
+              <span>Full time</span>
+            </JobTitleBlock>
+
+            <CreatedAt>
+              <ClockIcon />
+              <span>5 days ago</span>
+            </CreatedAt>
+
+            <JobInfo>
+              <CompanyLogoBlock>
+                <img
+                  src="https://kasisto.com/wp-content/themes/kasisto/img/client-logo.png"
+                  alt="Kasisto"
+                />
+              </CompanyLogoBlock>
+
+              <InfoBlock>
+                <h1>Kasisto</h1>
+
+                <LocationBlock>
+                  <PlanetIcon />
+                  <span>New York</span>
+                </LocationBlock>
+              </InfoBlock>
+            </JobInfo>
+          </JobHeader>
+
+          <JobDescription>
+            <p>Humanizing Digital Experiences®</p>
+            <p>
+              Kasisto’s Digital Experience Platform, KAI, is designed for financial
+              institutions to deliver the industry’s most amazing Conversational AI
+              powered intelligent virtual assistants to their customers. KAI is open
+              and extensible, and also fluent in the language of banking and
+              finance. From simple retail transactions to the complex demands of
+              corporate banks and wealth management, financial institutions can
+              deliver meaningful digital interactions with KAI that help build their
+              digital brand.
+            </p>
+
+            <p>
+              Financial institutions around the world use KAI, including DBS Bank,
+              J.P. Morgan, Mastercard, Standard Chartered, TD Bank, and Manulife
+              Bank among others. They chose KAI for its proven track record to drive
+              business results while improving customer experiences. The platform is
+              used by millions of consumers around the world, all the time, across
+              multiple channels, in different languages, and is optimized for
+              performance, scalability, security, and compliance.
+            </p>
+
+            <p>This position</p>
+
+            <p>
+              We are looking for a Full-Stack, client side software engineer to help
+              build and integrate responsive chat interfaces, analytics dashboards
+              and reporting tools.
+            </p>
+
+            <p>What you’ll be doing</p>
+
+            <p>
+              Working closely with clients and internal engineering, product and
+              design teams to gather requirements Building and integrating front-end
+              applications with CSS, HTML, Javascript, jQuery, Vue.js, Webpack,
+              Handlebars.js, LESS, Backbone, Python, Django and Java Working to
+              improve user experience and functionality for tools Writing testable
+              code utilizing common front-end unit and BDD testing frameworks What
+              you need for this position
+            </p>
+
+            <p>
+              3+ years in client-side web development with CSS, HTML, Javascript and
+              jQuery Proven, full-stack front-end experience using Python and Django
+              Other Modern Web Framework(s) experience is a plus (React, Vue,
+              Angular, Ember) Experience working collaboratively to build scalable,
+              modular, production software in an Agile environment Experience using
+              RESTful json services Node.js and API development familiarity is plus
+              D3.js is a plus
+            </p>
+
+            <p>What we offer:</p>
+
+            <p>
+              Competitive compensation package Ground floor opportunity within
+              rapidly growing tech startup Great collaborative team environment Full
+              Benefits Fun perks
+            </p>
+
+            <p>Location - NYC, Flatiron District</p>
+
+            <p>
+              We welcome your cover letter with a description of your previous
+              complete experience and your resume. Applicants must be authorized to
+              work in the US as we are unable to sponsor. Kasisto is an equal
+              opportunity employer.
+            </p>
+          </JobDescription>
+        </RightSide>
+      </Main>
+    </Container>
+  );
 };
 
 export default JobDetails;
--- src/pages/JobDetails/styles.js
+++ src/pages/JobDetails/styles.js
@@ -1,3 +1,232 @@
 import styled from 'styled-components';
+import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import Public from '@material-ui/icons/Public';
 
 export const Container = styled.div``;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 0.6rem;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+  margin-bottom: 1.8rem;
+
+  @media (min-width: 768px) {
+    width: 20%;
+    margin-right: 3.85rem;
+  }
+
+  > a {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+
+    svg {
+      transform: matrix(-1, 0, 0, 1, 0, 0);
+      fill: var(--color-blue);
+      margin-right: 0.55rem;
+      flex-shrink: 0;
+    }
+
+    span {
+      font-size: 0.7rem;
+      font-weight: 500;
+      font-family: 500;
+      line-height: 1.05rem;
+      color: var(--color-blue);
+    }
+
+    &:hover svg {
+      animation: pointing 1s ease-in-out infinite;
+    }
+
+    @keyframes pointing {
+      0%,
+      100% {
+        transform: matrix(-1, 0, 0, 1, 0, 0), translate(0, 0);
+      }
+
+      50% {
+        transform: matrix(-1, 0, 0, 1, 0, 0) translate(8px, 0);
+      }
+    }
+  }
+`;
+
+export const HowToApplyBlock = styled.div`
+  margin-top: 1.8rem;
+
+  h1 {
+    font-family: Poppins;
+    font-size: 0.7rem;
+    font-weight: 700;
+    line-height: 1.05rem;
+    text-transform: uppercase;
+
+    color: var(--color-text-secondary);
+  }
+
+  p {
+    margin-top: 0.8rem;
+
+    font-family: Poppins;
+    font-size: 0.7rem;
+    font-weight: 500;
+
+    color: var(--color-blue-dark);
+
+    a {
+      color: var(--color-blue);
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
+
+export const RightSide = styled.div`
+  @media (min-width: 768px) {
+    width: 80%;
+  }
+`;
+
+export const JobHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.6rem;
+`;
+
+export const JobTitleBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0.6rem;
+
+  h1 {
+    font-size: 1.2rem;
+    font-weight: 700;
+    line-height: 0.97rem;
+
+    color: var(--color-blue-dark);
+
+    margin-bottom: 0.5rem;
+  }
+
+  span {
+    max-width: max-content;
+    font-size: 0.6rem;
+    font-weight: 700;
+    color: var(--color-blue-dark);
+    line-height: 0.7rem;
+    padding: 0.3rem 0.4rem;
+    border: 1px solid var(--color-blue-dark);
+    border-radius: 0.2rem;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+
+    h1 {
+      margin-right: 0.85rem;
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export const CreatedAt = styled.div`
+  display: flex;
+  align-items: center;
+
+  svg {
+    width: 18px;
+    height: 18px;
+    margin-right: 0.425rem;
+    fill: var(--color-text-secondary);
+    flex-shrink: 0;
+  }
+
+  span {
+    font-size: 0.7rem;
+    font-weight: 500;
+    line-height: 0.7rem;
+    color: var(--color-text-secondary);
+  }
+`;
+
+export const JobInfo = styled.div`
+  margin-top: 1.85rem;
+  display: flex;
+`;
+
+export const CompanyLogoBlock = styled.div`
+  width: 42px;
+  height: 42px;
+  margin-right: 0.7rem;
+
+  img {
+    object-fit: cover;
+    border-radius: 0.2rem;
+    height: 100%;
+  }
+`;
+
+export const InfoBlock = styled.div`
+  h1 {
+    font-size: 0.8rem;
+    font-weight: 700;
+    line-height: 1.05rem;
+    color: var(--color-blue-dark);
+  }
+`;
+
+export const LocationBlock = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 0.36rem;
+
+  svg {
+    width: 15px;
+    height: 15px;
+    fill: var(--color-text-secondary);
+    flex-shrink: 0;
+
+    margin-right: 0.375rem;
+  }
+
+  span {
+    font-size: 0.6rem;
+    font-weight: 500;
+    line-height: 0.7rem;
+    color: var(--color-text-secondary);
+  }
+`;
+
+export const JobDescription = styled.div`
+  p {
+    color: var(--color-blue-dark);
+    margin-bottom: 1.2rem;
+    font-weight: 400;
+    font-size: 0.8rem;
+    line-height: 1.2rem;
+  }
+`;
+
+export const PlanetIcon = styled(Public)``;
+
+export const ClockIcon = styled(AccessTimeIcon)``;
+
+export const ArrowBack = styled(TrendingFlatIcon)``;
